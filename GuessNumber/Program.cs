53ca8f4d706fbf8ce1f8ca0e0b28b4c2259c8749
Mukarram Ali <info.mukarramali@gmail.com>
2025-05-08 13:23:09 +0500

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class GuessTheNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Guess the Number Game ===");
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 13); // 1 to 12
+        int guess;
+        int attempts = 0;
+
+        do
+        {
+            Console.Write("Guess a number between 1 and 12: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            attempts++;
+
+            if (guess < numberToGuess)
+                Console.WriteLine("Too Low!");
+            else if (guess > numberToGuess)
+                Console.WriteLine("Too High!");
+            else
+                Console.WriteLine($"Correct! You Guessed it in {attempts} attempts.");
+        } while (guess != numberToGuess);
+    }
+}
